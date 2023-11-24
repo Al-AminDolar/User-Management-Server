c27cbd8af5f69c36200c5b4fca3a776ddc2fa1ac
alamin <alamin.dolar1@gmail.com>
2023-11-24 21:10:13 +0600

    client to server data sent and data display

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+// middlwwae
+app.use(cors());
+app.use(express.json());
+
+// Local Database
+const users = [
+  { id: 1, name: "alamin", email: "alamin@gmail.com" },
+  { id: 2, name: "alom", email: "alom@gmail.com" },
+  { id: 3, name: "alim", email: "alim@gmail.com" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Users Manegment Server");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
+  console.log("Post api hitting");
+  res.send(user);
+  console.log(user);
+});
 
 app.listen(port, () => {
-  app.get("/", (req, res) => {
-    res.send("Users Manegment Server");
-  });
   console.log(`App is running on ${port}`);
 });
